cdeec3a5830df4e46c8f6f764ceda764db251d5a
Auroprasadmaharana <maharanaauro23@gmail.com>
2025-07-16 12:49:53 +0530

    Delete models directory

--- models/other_input.py
+++
@@ -1,7 +0,0 @@
-class OtherInput(models.Model):
-    _name = 'other.input'
-    _description = 'Other Input'
-
-    name = fields.Char(string='Name', required=True)
-    description = fields.Text(string='Description')
-    amount = fields.Float(string='Amount')
--- models/working_schedule.py
+++
@@ -1,7 +0,0 @@
-# models/working_schedule.py
-from odoo import models, fields
-
-class WorkingSchedule(models.Model):
-    _inherit = 'resource.calendar'
-
-    description = fields.Text(string='Description')
